3c461030acd2b2bdc56fb643dfdf66b81151a6f9
rinse <rinse@neko2.net>
2020-12-20 15:36:01 +0900

    add cognito.yml

--- deploy.md
+++ deploy.md
@@ -1,14 +1,27 @@
-1. Run the following.
+# How to deploy on your local 
+
+1. Deploy cognito
 
 ```bash
-cloudfront/deploy.bash lambdaedge
+infra/deploy.bash cognito ENVIRONMENT_NAME DOMAIN_NAME
 ```
 
-2. Go to [certificate Manager](https://console.aws.amazon.com/acm/home?region=us-east-1#/) and note the Arn.
-
-3. Run the following.
+2. Deploy lambdaedge
 
 ```bash
-cloudfront/deploy.bash cloudfront ${YOUR_CERTIFICATE_ARN}
+infra/deploy.bash lambdaedge ENVIRONMENT_NAME DOMAIN_NAME
+```
+
+3. Go to [certificate Manager](https://console.aws.amazon.com/acm/home?region=us-east-1#/) and note the Arn.
+
+4. Deploy cloudfront
+
+```bash
+infra/deploy.bash cloudfront ENVIRONMENT_NAME DOMAIN_NAME ${YOUR_CERTIFICATE_ARN}
+```
+
+5. Upload frontend code
+
+```bash
 cd uchiyoso_logs && npm run deploy
 ```
--- infra/deploy.bash
+++ infra/deploy.bash
@@ -1,30 +1,48 @@
 #!/bin/bash
 set -eu
 
-SERVICE=$1
-
 function usage() {
-    echo 'Usage: deploy.bash (cloudfront|lambdaedge)'
+    echo 'Usage: deploy.bash SERVICE [OPTIONS...]'
+    echo '  SERVICE: cognito    ENVIRONMENT_NAME DOMAIN_NAME'
+    echo '           lambdaedge ENVIRONMENT_NAME DOMAIN_NAME'
+    echo '           cloudfront ENVIRONMENT_NAME DOMAIN_NAME CERTIFICATE_ARN'
 }
 
-if [ $SERVICE = "cloudfront" ]; then
-    CERTIFICATE_ARN=$2
+service=$1
+if [ $3 = "" ]; then
+    environmentName=""
+else
+    environmentName=${2:+"-$2"}
+fi
+domainName=$3
+
+if [ $service = "cloudfront" ]; then
+    CERTIFICATE_ARN=$4
     aws cloudformation deploy \
-        --stack-name drinkbar-stack \
-        --template-file cloudfront.yml \
+        --stack-name drinkbar-stack${environmentName} \
+        --template-file cloudfront/cloudfront.yml \
         --parameter-overrides \
-            BucketName=drinkbar.esnir-nzmz.com \
-            DomainAlias=drinkbar.esnir-nzmz.com \
+            BucketName=$domainName \
+            DomainAlias=$domainName \
             LambdaEdgeFunctionName=drinkbarBasicAuthentication \
             LambdaEdgeFunctionVersion=2 \
             CertificateArn=$CERTIFICATE_ARN
-elif [ $SERVICE = "lambdaedge" ]; then
+elif [ $service = "lambdaedge" ]; then
     aws cloudformation --region us-east-1 deploy \
-        --stack-name drinkbar-stack \
+        --stack-name drinkbar-stack${environmentName} \
         --capabilities CAPABILITY_NAMED_IAM \
-        --template-file lambdaedge.yml \
+        --template-file cloudfront/lambdaedge.yml \
         --parameter-overrides \
-            DomainName=drinkbar.esnir-nzmz.com
+            domainName=$domainName
+elif [ $service = "cognito" ]; then
+    aws cloudformation deploy \
+        --stack-name drinkbar-cognito-stack${environmentName} \
+        --template-file cognito.yml \
+        --parameter-overrides \
+            CallbackURL=$domainName \
+            DefaultRedirectURI=$domainName \
+            LogoutURL=$domainName \
+            EnvironmentName=$environmentName
 else
     usage
 fi
